4a314ce46f1037b4b015010bb25b3ca78a9e21de
jc <jc.chalte@gmail.com>
2015-01-23 21:46:14 +0100

    correction serveur

--- Server.js
+++ Server.js
@@ -1,11 +1,11 @@
 ﻿/// <reference path="Scripts/GlobalReferences.d.ts"/>
-var server = require("src/server/server");
+var server = require("./src/server/server");
 
 (function () {
     var myServer = new server();
 
-    var port = process.env.PORT || Number(process.argv[2]);
+    var port = process.env.PORT || Number(process.argv[2]) || 8080;
     myServer.start(port, "src/server/Views/homepage.html", "src/server/Views/404.html", function () {
-        console.log("Server Ready to serve...");
+        console.log("Server Ready to serve on port " + port + "...");
     });
 })();
--- Server.ts
+++ Server.ts
@@ -1,13 +1,12 @@
 ﻿/// <reference path="Scripts/GlobalReferences.d.ts"/>
 
-import server = require("src/server/server");
+import server = require("./src/server/server");
 
 (() => {
     var myServer = new server();
 
-
-    var port = process.env.PORT || Number(process.argv[2]);
+    var port = process.env.PORT || Number(process.argv[2]) || 8080;
     myServer.start(port, "src/server/Views/homepage.html", "src/server/Views/404.html", () => {
-        console.log("Server Ready to serve...");
+        console.log("Server Ready to serve on port "+port+"...");
     });
 })();
